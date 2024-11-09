fe7232e0a91a4ac7b395526ca650715e2da69d2d
Zack Birkenbuel <zack@emailzack.com>
2024-11-09 10:45:17 -0800

    Better coverage

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 const PROCESSED = Symbol("walker processed");
 
+const TEXT_REPLACEMENTS = [
+  {re: /Donald Trump/gi, to: 'Orange Man'},
+  {re: /Trump/gi, to: 'Orange Man'},
+  {re: /Elon Musk/gi, to: 'Some Guy'},
+  {re: /Elon/gi, to: 'Some Guy'},
+  {re: /Musk/gi, to: 'Some Guy'},
+  {re: /RFK/gi, to: 'Wormtail'},
+  {re: /Kennedy/gi, to: 'Wormtail'},
+  {re: /JD Vance/gi, to: 'Sofa King'},
+  {re: /J\.D\. Vance/gi, to: 'Sofa King'},
+  {re: /Vance/gi, to: 'Sofa King'},
+]
+
+
 function performTextHole(text) {
-  return (text || "").replaceAll(/trump/gi, "ASSHOLE");
+  let txt = text || "";
+  for (const replacement of TEXT_REPLACEMENTS) {
+    text = text.replaceAll(replacement.re, replacement.to);
+  }
+  return text;
 }
 
 function shouldHole(text) {
-  return (text || "").toLowerCase().includes("trump")
+  // just if any will match, some should short circuit and not excute them all.
+  return TEXT_REPLACEMENTS.some(replacement => replacement.re.test(text));
+}
+
+function performImageHole(img) {
+  if (img[PROCESSED]) {
+    return;
+  }
+  img.setAttribute("style", "filter: blur(1em);");
+  img[PROCESSED] = true;
+
 }
 
 function holeSimilarLinks(global, node) {
@@ -17,9 +45,15 @@ function holeSimilarLinks(global, node) {
       // find all of the similar links
       const similar = global.document.querySelectorAll(`a[href="${href}"]`);
       for (const sim of similar) {
+        // make the link not work
+        sim.setAttribute("data-original-href", href);
+        sim.setAttribute("href", "#");
+        // Mayyyybe there are images that weren't caught previously, blur them too?
         // Be better
-        sim.setAttribute("style", "border:4px solid magenta!");
-        node[PROCESSED] = true;
+        for (const img of sim.querySelectorAll("img")) {
+          performImageHole(img);
+        }
+        sim[PROCESSED] = true;
       }
     }
   }
@@ -29,7 +63,6 @@ async function performMemoryHole(global) {
   // walk everything looking for the thing to memory hole
   const textWalker = global.document.createTreeWalker(global.document, NodeFilter.SHOW_TEXT, {
     acceptNode(node) {
-      console.log(node);
       if (node[PROCESSED]) {
         return NodeFilter.FILTER_SKIP;
       }
@@ -44,7 +77,7 @@ async function performMemoryHole(global) {
     node[PROCESSED] = true;
     // replace the string
     node.textContent = performTextHole(node.textContent);
-    // perform the link matching magic
+    // Find everything that shares a link
     holeSimilarLinks(global, node.parentElement);
   }
 
@@ -52,25 +85,30 @@ async function performMemoryHole(global) {
   const imgWalker = global.document.createTreeWalker(global.document, NodeFilter.SHOW_ELEMENT, {
     acceptNode(node) {
       if (node[PROCESSED]) {
-        return NodeFilter.FILTER_SKIP;
+        return NodeFilter.FILTER_REJECT;
       }
+      // TODO, support for picture and figure?
       if (node.nodeName.toLowerCase() !== "img") {
-        return NodeFilter.FILTER_REJECT
+        return NodeFilter.FILTER_SKIP
       }
-      // just check alt text for now, could maybe look at URLs?
+      // Alt text for sure since news sources should alt text everything.  Maybe SRCs too?
+      // TODO: support for source sets.
+      if (shouldHole(node.getAttribute("src"))) {
+        return NodeFilter.FILTER_ACCEPT;
+      }
       return shouldHole(node.getAttribute("alt")) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
     }
   });
   
-    
+  // TODD: Veeery similar to the above, so is the walker for that matter, could maybe refactor.
   while (imgWalker.nextNode()) {
     const node = imgWalker.currentNode;
     // mark the node as processed so it isn't scanned again
+    console.log(node);
     node[PROCESSED] = true;
     // replace the image
-    node.setAttribute("alt", "SOME ASSHOLE");
-    
-    // perform the link matching magic
+    performImageHole(node);
+    // Find everything that shares a link
     holeSimilarLinks(global, node);
   }
 }
